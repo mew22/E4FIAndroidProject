b8536b56424396c11a071fdabc44d5f79193a66a
Coorjet <kevin.lof@edu.esiee.fr>
2015-12-03 19:56:11 +0100

    Commit - Profile Update
    [19:52:16] Kev': Bon
    [19:52:20] Kev': Le profil est updatable
    [19:52:31] Kev': Avev vérif de si on a déjà rentré quelquechose ou pas
    [19:52:36] Kev': ça se met dans la BDD et ça s'actu
    [19:52:42] Kev': et quand on se reco, ça ressort le profil
    [19:53:26] Kev': Donc hormis quelques bidouillages d'ordre pratique à
    faire
    [19:54:40] Kev': (Dans l'ordre, finir de faire le forcing full filed,
    aka remplir tous les champs pour avoir l'édition ( ça je m'en
    occuperais) ,
    Mettre des majuscules au début des champs automatiquement,
    Design la page)
    [19:54:43] Kev': le profil marche

--- JamTogether/app/src/main/java/com/example/kvin/jamtogether/MainActivity.java
+++ JamTogether/app/src/main/java/com/example/kvin/jamtogether/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kvin.jamtogether;
 
+import android.app.Dialog;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +8,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.ui.ParseLoginBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,8 +39,19 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         TextView txt_welc = (TextView) findViewById(R.id.txt_v_welcome);
-        txt_welc.setText( "Salut  !" +ParseUser.getCurrentUser().get("username"));
+        txt_welc.setText("Salut, " + ParseUser.getCurrentUser().get("username") + " !");
         txt_welc.setTextColor(Color.WHITE);
+        txt_welc.setTextSize(25);
+
+        Button btn_prof = (Button) findViewById(R.id.button_profil);
+
+        btn_prof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            openProfile(v);}
+
+        });;
     }
 
     @Override
@@ -59,17 +77,116 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
     public void openProfile(View v){
 
         setContentView(R.layout.profile);
 
-       // Button btneditprf = (Button) findViewById(R.id.btneditprofile);
-       // Button btnretacc = (Button) findViewById(R.id.btnretacc);
+        Button btneditprf = (Button) findViewById(R.id.butEditProfile);
+        Button btnretacc = (Button) findViewById(R.id.butReturnWelc);
+
+        TextView tvpseudo = (TextView) findViewById(R.id.tvPseudoEdit);
+        TextView tvage = (TextView) findViewById(R.id.tvAgeEdit);
+        TextView tvPrenom = (TextView) findViewById(R.id.tvPrenomEdit);
+        TextView tvlocation = (TextView) findViewById(R.id.tvLocationEdit);
+        TextView tvinstru = (TextView) findViewById(R.id.tvInstrumentPrincEdit);
+
+        // Histoire de check si on l'a déjà fait ou pas ( la vérif full field plus bas empêche de se marcher dessus)
+if(ParseUser.getCurrentUser().get("age") != null) {
+    tvpseudo.setText(ParseUser.getCurrentUser().get("username").toString());
+    tvage.setText(ParseUser.getCurrentUser().get("age").toString());
+    tvPrenom.setText(ParseUser.getCurrentUser().get("name").toString());
+    tvlocation.setText(ParseUser.getCurrentUser().get("location").toString());
+    tvinstru.setText(ParseUser.getCurrentUser().get("main_instrument").toString());
+}
+
+
+        btneditprf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.edit_profile_dialog);
+                dialog.setCancelable(true);
+
+                final EditText editpseudo = (EditText) dialog.findViewById(R.id.editpseudo);
+                final EditText editprenom = (EditText) dialog.findViewById(R.id.edit_prenom);
+                final EditText editage = (EditText) dialog.findViewById(R.id.edit_age);
+                final EditText editlocation = (EditText) dialog.findViewById(R.id.edit_location);
+                final EditText editinst = (EditText) dialog.findViewById(R.id.edit_main_inst);
+
+                final TextView error_text = (TextView) dialog.findViewById(R.id.texterroredit);
+
+                Button confirm = (Button) dialog.findViewById(R.id.button_confirm_edition);
+                Button cancel = (Button) dialog.findViewById(R.id.button_cancel_edition);
+
+                final List<EditText> listeditprofile = new ArrayList<EditText>();
 
 
 
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listeditprofile.add(editpseudo);
+                        listeditprofile.add(editage);
+                        listeditprofile.add(editprenom);
+                        listeditprofile.add(editlocation);
+                        listeditprofile.add(editinst);
 
 
+                        //Test de remplissage
+                       if (listeditprofile.contains(null)|| listeditprofile.contains("")){
+
+                           error_text.setText("Please fill in all the fields.");
+                           error_text.setTextColor(Color.RED);
+
+                       }
+
+                        else {
+
+                           // Actu dans la BDD
+
+                           ParseUser.getCurrentUser().put("username",editpseudo.getText().toString());
+                           ParseUser.getCurrentUser().put("age", editage.getText().toString());
+                           ParseUser.getCurrentUser().put("name",editprenom.getText().toString());
+                           ParseUser.getCurrentUser().put("location",editlocation.getText().toString());
+                           ParseUser.getCurrentUser().put("main_instrument",editinst.getText().toString());
+
+                           ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+                               public void done(com.parse.ParseException e) {
+                                   if (e == null) {
+                                       // Save was successful!
+                                      Toast.makeText(MainActivity.this,"Profile Updated !", Toast.LENGTH_LONG).show();
+                                   } else {
+                                       //Failed
+                                       Toast.makeText(MainActivity.this,"Error during Update !", Toast.LENGTH_LONG).show();
+                                   }
+                               }
+                           });
+
+                       }
+
+                        //Réactualisation du Profil
+                        dialog.dismiss();
+                        openProfile(v);
+                    }
+
+                });
+
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    dialog.dismiss();
+                        openProfile(v);
+
+                    }
+
+                });
+                dialog.show();
+            }
+
+        });
 
 
 
